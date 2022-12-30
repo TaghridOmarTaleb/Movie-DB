3490bb07507ef7a192be2772a1ed9e69eb0df1e8
taghrid <taghridtaleb1@gmail.com>
2022-12-30 20:28:15 +0200

    step3

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ app.get('/npmjs.org', function (req, res) {
   res.send('ok')
 })
 
+app.get('/time', function (req, res) {
+  res.send("status:200, message: it's "+ currenttime)
+})
+
+app.get('/test', function (req, res) {
+  res.send('{status:200, message:"ok"}')
+  
+})
+let time = new Date();
+let currenttime = time.getHours()+
+              //":"+time.getMinutes()+
+              ":"+ time.getSeconds();
+
 app.listen(3000)
